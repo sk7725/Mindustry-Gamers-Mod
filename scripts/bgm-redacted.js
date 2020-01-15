3391a68662331f04565e5be38d14bd1ca9e7c44b
sk7725 <58885089+sk7725@users.noreply.github.com>
2020-01-15 19:30:41 +0900

    Update bgm-redacted.js

--- scripts/bgm-redacted.js
+++ scripts/bgm-redacted.js
@@ -1,28 +1,14 @@
-var region = Core.atlas.find("bgm-redacted");
+//var region = Core.atlas.find("bgm-redacted");
 print('Loading Music Block...')
 
 const bgmblock = extendContent(genericSmelter, "bgm-redacted", {
-	//init(){
-	//	this.itSpin = 2; // it spin
-	//},
 	buildConfiguration(tile, table){
 		table.addImageButton(Icon.arrowUpSmall, Styles.clearTransi, run(() => {
-			// Tell client to spin faster
-			tile.configure(1);
+			tile.configure(0);
 		})).size(50);
-		//table.row();
-		//table.addImageButton(Icon.arrowDownSmall, Styles.clearTransi, run(() => {
-		//	// Tell client to spin slower
-		//	tile.configure(-1);
-		//})).size(50);
 	},
 
 	configured(tile, player, value){
-		if(value != -1){
-			value = 1;
-		}
-
-		//this.itSpin += value;
 	},
 
 	//draw(tile){
@@ -30,8 +16,8 @@ const bgmblock = extendContent(genericSmelter, "bgm-redacted", {
 	//}
 });
 
-spinner.localizedName = "Music Block - REDACTED";
-spinner.description = "Iㅜ'ㄹ RUDㅌ ㅜㅇ TAㄴK ABㅇUㅜ ㄹOMEㅇNㅌ Wㅐㅇ'ㄹ ㄴㅣSTㅌNㅣNG";
-spinner.configurable = true;
+bgmblock.localizedName = "Music Block - REDACTED";
+bgmblock.description = "Iㅜ'ㄹ RUDㅌ ㅜㅇ TAㄴK ABㅇUㅜ ㄹOMEㅇNㅌ Wㅐㅇ'ㄹ ㄴㅣSTㅌNㅣNG";
+bgmblock.configurable = true;
 
 print("Created Music Block")
